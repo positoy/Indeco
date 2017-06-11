a56a8cc51de2ce261a4b824d183c15b6bed65a1c
positoy <positoy@gmail.com>
2017-06-11 19:31:08 +0900

    initial website (PhotoSwipe directory removed)

---
+++ website/web_res/gallery_loader.js
@@ -0,0 +1,12 @@
+var gallery_files = [
+    'NEOTEX52555.jpg',
+    'NEOTEX52556.jpg',
+    'NEOTEX52562.jpg',
+    'NEOTEX52591.jpg',
+    'NEOTEX52592.jpg',
+    'NEOTEX52590.jpg',
+    'NEOTEX52585.jpg',
+    'NEOTEX52584.jpg',
+    'NEOTEX52583.jpg',
+    'NEOTEX52581.jpg',
+];
